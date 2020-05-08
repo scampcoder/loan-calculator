a9c459d729f1b01bdd118c9b34b59d21e03f4376
Victoria Kee <victoriakee95@gmail.com>
2020-05-08 11:51:14 -0500

    Fix math error for payment var

--- app.js
+++ app.js
@@ -3,4 +3,11 @@ function computeLoan() {
   let interest_rate = document.getElementById('interest_rate').value;
   let months = document.getElementById('months').value;
   let interest = (amount * (interest_rate * .01)) / months;
+  let payment = ((amount/months) + interest).toFixed(2);
+  //place commas on 3-digit numbers i.e. 3,000
+  //"/B" used to look inside word boundary
+  //"?" finds the group (in this case groups of 3 digits('\d{3}'))
+  payment = payment.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  //place payment var inside HTML
+  document.getElementById('payment').innerHTML = "Monthly Payment = $"+payment;
 }
